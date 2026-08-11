26e6c7998ffb164c54cbba573d02389d0a1ece30
Vishnu Vardhan Malempati <238T1A0597@dietonline.in>
2026-08-11 23:36:10 +0530

    Time caluclation

---
+++ rs_cse_597_1_08.py
@@ -0,0 +1,6 @@
+m = int(input('Enter Minutes to convert seconds: '))
+s = int(input('Enter Hours to convert minutes: '))
+h = m * 60
+Mi = s * 60
+print(m , 'Minutes =', h ,'Seconds')
+print(s , 'Hours =' , Mi ,'Minutes')
