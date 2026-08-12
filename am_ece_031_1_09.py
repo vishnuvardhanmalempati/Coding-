275a1c53aa6510d14c4abd807e14f8bb4eb1ad7c
krishna Kishore <tkittu88@gmail.com>
2026-08-12 11:45:26 +0530

    Caluclator

---
+++ am_ece_031_1_09.py
@@ -0,0 +1,12 @@
+a = int(input('Enter A value:'))
+b = int(input('Enter B value:'))
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+g = a % b
+print('Addition:', c)
+print('Subtraction',d)
+print('Multiplication',e)
+print('Division',f)
+print('Modulus',g)
