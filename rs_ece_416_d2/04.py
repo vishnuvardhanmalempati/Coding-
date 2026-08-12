7f02a2789e7ab4d6a1fe2dfc7dd9856ff9be305e
Sathwika_0410 <sathwikanaidu04.b@gmail.com>
2026-08-13 00:20:06 +0530

    Add files via upload

---
+++ rs_ece_416_d2/04.py
@@ -0,0 +1,5 @@
+age = int(input("Enter your age: "))
+
+result = "Eligible to vote." if age >= 18 else "Not eligible to vote."
+
+print(result)
---
+++ rs_ece_416_d2/05.py
@@ -0,0 +1,3 @@
+for i in range(1, 12):
+    if i < 11:
+        print(i)
